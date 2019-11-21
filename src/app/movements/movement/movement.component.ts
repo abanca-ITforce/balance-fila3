a50f2d3a6e71f5d38dfb269855c86b4c548999cb
eloygperez <eloy.gperez@gmail.com>
2019-11-21 20:38:36 +0100

    actualizacion

--- src/app/movements/movement/movement.component.ts
+++ src/app/movements/movement/movement.component.ts
@@ -8,10 +8,39 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MovementComponent implements OnInit {
 
+  movimiento;
+
   movementDescription: string;
+  movimientos = [
+    {
+      id: 0,
+      concepto: 'Pago de curso',
+      cantidad: 100,
+      tipo: 'gasto'
+    },
+    {
+      id: 1,
+      concepto: 'Arreglar bicicleta',
+      cantidad: 89,
+      tipo: 'gasto'
+    },
+    {
+      id: 2,
+      concepto: 'Contrato Abanca',
+      cantidad: 2500,
+      tipo: 'ingreso'
+    },
+    {
+      id: 3,
+      concepto: 'Participaciones Banco Santander',
+      cantidad: 9000,
+      tipo: 'ingreso'
+    }
+  ];
 
   constructor(activatedRoute: ActivatedRoute) {
-    this.movementDescription = activatedRoute.snapshot.params.id;
+    // this.movementDescription = activatedRoute.snapshot.params.id;
+    this.movimiento = this.movimientos[activatedRoute.snapshot.params.id];
    }
 
   ngOnInit() {
--- src/app/movements/movements.component.ts
+++ src/app/movements/movements.component.ts
@@ -6,7 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movements.component.css']
 })
 export class MovementsComponent implements OnInit {
-
+  movimientos = [
+    {
+      id: 0,
+      concepto: 'Pago de curso',
+      cantidad: 1,
+      tipo: 'gasto'
+    },
+    {
+      id: 1,
+      concepto: 'Arreglar bicicleta',
+      cantidad: 89,
+      tipo: 'gasto'
+    },
+    {
+      id: 2,
+      concepto: 'Contrato Abanca',
+      cantidad: 2500,
+      tipo: 'ingreso'
+    },
+    {
+      id: 3,
+      concepto: 'Participaciones Banco Santander',
+      cantidad: 9000,
+      tipo: 'ingreso'
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
